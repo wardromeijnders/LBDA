e37725ac29583846e576f74544ffd9b501a94793
Niels Wouda <nielswouda@gmail.com>
2019-12-12 16:49:51 +0100

    Add header guards for run.h

--- run.h
+++ run.h
@@ -1,3 +1,11 @@
+#ifndef RUN_H
+#define RUN_H
+
+
+#include "smips/problem_data/data.h"
+#include "smips/problem_data/problem.h"
+
+
 void run_ssv(Data &rand,
              GRBEnv &env,
              GRBenv *c_env,
@@ -8,3 +16,5 @@ void run_ssv(Data &rand,
 void run_sslp(Data &rand, GRBEnv &env, GRBenv *c_env);
 
 void run_sizes(Data &rand, GRBEnv &env, GRBenv *c_env);
+
+#endif  // RUN_H
