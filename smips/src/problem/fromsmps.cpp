a46b0792b8b4b6eba213f3b6172ee7bce4498347
Niels Wouda <nielswouda@gmail.com>
2020-04-25 17:12:09 +0200

    Read several SMIPS data fields

--- smips/src/problem/fromsmps.cpp
+++ smips/src/problem/fromsmps.cpp
@@ -13,13 +13,44 @@ Problem Problem::fromSmps(char const *location, GRBEnv &env)
 
     SmiCoreData *core = smi.getCore();
 
-    if (core->getNumStages() > 2)
+    size_t nStages = core->getNumStages();
+    size_t nScenarios = smi.getNumScenarios();
+
+    if (nStages > 2)
         throw "Does not support multi-stage models.";
 
-    assert(core->getNumStages() == 2);
-    assert(smi.getNumScenarios() > 0);
+    assert(nStages == 2);
+    assert(nScenarios > 0);
 
-    // TODO
+    // TODO rename these once I have a better understanding of their contextual
+    //  meaning.
+    auto nRows = arma::Col<int>(nStages);
+    auto nCols = arma::Col<int>(nStages);
+    auto nInts = arma::Col<int>(nStages);
+    auto rStart = arma::Col<int>(nStages);
+    auto cStart = arma::Col<int>(nStages);
+
+    for (size_t stage = 0; stage != nStages; ++stage)
+    {
+        nRows[stage] = core->getNumRows(stage);
+        nCols[stage] = core->getNumCols(stage);
+        nInts[stage] = core->getIntCols(stage).size();
+
+        rStart[stage] = core->getRowStart(stage);
+        cStart[stage] = core->getColStart(stage);
+
+        // TODO core data
+    }
+
+    auto probs = arma::vec(nScenarios);
+
+    for (size_t scenario = 0; scenario != nScenarios; ++scenario)
+    {
+        auto scnNode = smi.getLeafNode(scenario);
+        probs[scenario] = scnNode->getProb();
+
+        // TODO scenario data
+    }
 
     return Problem{env};
 }
