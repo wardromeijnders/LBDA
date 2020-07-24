dce8d84e09778d3c43ad277f3708452bcd27a057
Niels Wouda <nielswouda@gmail.com>
2020-07-24 22:17:01 +0200

    Stubs for initial data tests

---
+++ smips/test/test_lands.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+
+
+TEST(TestDataLandS, LpDual)
+{
+    // TODO
+}
+
+TEST(TestDataLandS, LooseBenders)
+{
+    // TODO
+}
+
+TEST(TestDataLandS, StrongBenders)
+{
+    // TODO
+}
---
+++ smips/test/test_sslp.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+
+TEST(TestDataSSLP_5_25_50, LpDual)
+{
+    // TODO
+}
+
+TEST(TestDataSSLP_5_25_50, StrongBenders)
+{
+    // TODO
+}
+
+TEST(TestDataSSLP_5_25_50, LooseBenders)
+{
+    // TODO
+}
+
+
+// TODO larger instances
